18f6de0b3e10e9627a9a777e3cd5f5ad0813ccdd
Sergio Mayoral Martinez <sergiomayoralmartinez@gmail.com>
2020-03-09 00:22:06 +0100

    Added auto-disconnect after 10seconds of the last connection, which forces a re-connect the next time a tv button is pressed Added a ping check to determine TV state. This would fix #1

--- custom_components/samsungtv_encrypted/PySmartCrypto/pysmartcrypto.py
+++ custom_components/samsungtv_encrypted/PySmartCrypto/pysmartcrypto.py
@@ -6,11 +6,14 @@ from .command_encryption import AESCipher
 import requests
 import time
 import websocket
+import threading
 
 class PySmartCrypto():
     UserId = "654321"
     AppId = "12345"
     deviceId =  "7e509404-9d7c-46b4-8f6a-e2a9668ad184"
+
+    def disconnectCallback(self): self.close()
 
     def getFullUrl(self, urlPath):
         return "http://" + self._host + ":" + self._port + urlPath
@@ -118,6 +121,9 @@ class PySmartCrypto():
         self._host = host
         self._port = port
         self._connection = self.connect()
+
+        self._timer = threading.Timer(10, self.disconnectCallback)
+        self._timer.start()
 
         if token is None and sessionid is None:
             self.StartPairing()
